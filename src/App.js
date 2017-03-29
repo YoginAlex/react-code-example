e5e25621f80a2e0ba71f90d810b9870181e7d96f
Alexey Zakharenko <yogin.alex@gmail.com>
2017-03-29 20:07:29 +0300

    [add] auto create notification

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { compose } from 'recompose';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import { Header, Actions } from './components';
+import { addNotification } from './store/notifications';
 
 import './App.css';
 
@@ -11,18 +16,39 @@ injectTapEventPlugin();
 
 
 class App extends Component {
-  render() {
-    return (
-        <MuiThemeProvider>
-            <div>
-                <Header />
-                <div className="content">
-                    <Actions />
+    static propTypes = {
+        addNotification: PropTypes.func.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+
+        setInterval(() => {
+            this.props.addNotification(`Notification at ${new Date()}`);
+        }, 15000);
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div>
+                    <Header />
+                    <div className="content">
+                        <Actions />
+                    </div>
                 </div>
-            </div>
-        </MuiThemeProvider>
-    );
-  }
+            </MuiThemeProvider>
+        );
+    }
 }
 
-export default App;
+const decorate = compose(
+    connect(
+        null,
+        dispatch => bindActionCreators({
+            addNotification
+        }, dispatch)
+    )
+);
+
+export default decorate(App);
